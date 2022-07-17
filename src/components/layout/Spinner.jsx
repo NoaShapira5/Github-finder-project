d95d05b79b873b92fac86d9a21938a0b28e76f52
noa0098 <106147336+NoaShapira5@users.noreply.github.com>
2022-07-17 13:39:10 +0300

    .

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,15 @@
+import spinner from './assets/spinner.gif'
+
+function Spinner() {
+  return (
+    <div>
+      <img
+        width={180}
+        src={spinner}
+        alt='Loading...'
+      />
+    </div>
+  )
+}
+
+export default Spinner
